7db8c9c6696cf6087aee2941ffab51e5b4ce77d9
puiusabin <puiumihaisabin@gmail.com>
2025-12-15 14:05:29 +0200

    chore(web): remove storybook and stories
    - Deleted all *.stories.tsx files
    - Removed .storybook configuration directory
    - Removed storybook dependencies and scripts from package.json

--- apps/web/.storybook/mocks/handlers.ts
+++
@@ -1,5 +0,0 @@
-import { authHandlers } from "./auth"
-import { mailHandlers } from "./mail"
-import { web3Handlers } from "./web3"
-
-export const handlers = [...authHandlers, ...mailHandlers, ...web3Handlers]
